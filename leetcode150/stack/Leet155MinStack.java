43ea91ef4f45ef780aa21231bec75d059042a1cb
Rishi Gupta <rishigupta17sep1996@gmail.com>
2025-12-18 10:13:41 +0530

    refactor

--- leetcode150/stack/Leet155MinStack.java
+++ leetcode150/stack/Leet155MinStack.java
@@ -1,5 +1,7 @@
 package stack;
 
+import utils.Pair;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,5 +18,40 @@ public class Leet155MinStack {
         System.out.println(stack.top());
         System.out.println(stack.getMin());
 
+    }
+
+}
+
+class MinStack {
+
+    List<Pair> list = new ArrayList<>();
+
+    public MinStack() {
+
+    }
+
+    public void push(int val) {
+        int min = val;
+        if (!list.isEmpty()) {
+            min = Math.min(val, topElement().getMin());
+        }
+
+        list.add(new Pair(val, min));
+    }
+
+    public void pop() {
+        list.remove(list.size() - 1);
+    }
+
+    public int top() {
+        return topElement().getVal();
+    }
+
+    public Pair topElement() {
+        return list.get(list.size() - 1);
+    }
+
+    public int getMin() {
+        return topElement().getMin();
     }
 }
